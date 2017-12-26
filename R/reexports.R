2b66d4b3a7961545ea1bab4f52f84b45fbd9e5fd
earowang <earo.wang@gmail.com>
2017-12-26 23:33:25 +1100

    depends on latest pillar and tsibble

--- R/reexports.R
+++ R/reexports.R
@@ -9,3 +9,12 @@ pillar::type_sum
 #' @importFrom pillar obj_sum
 #' @export
 pillar::obj_sum
+
+#' @importFrom pillar is_vector_s3
+#' @export
+pillar::is_vector_s3
+
+
+#' @importFrom pillar pillar_shaft
+#' @export
+pillar::pillar_shaft
--- R/type-sum.R
+++ R/type-sum.R
@@ -12,3 +12,14 @@ tbl_sum.range <- function(x) {
 obj_sum.range <- function(x) {
   compact_range(x)
 }
+
+#' @export
+is_vector_s3.range <- function(x) {
+  TRUE
+}
+
+#' @export
+pillar_shaft.range <- function(x, ...) {
+  out <- compact_range(x)
+  pillar::new_pillar_shaft_simple(out, align = "right")
+}
